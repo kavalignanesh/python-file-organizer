33cb761fea66dc24dfd391f2b01214fcc6bcda04
Gnanesh kavali <kavalignanesh@gmail.com>
2026-04-21 17:38:39 +0530

    initial commit

---
+++ organizer.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+path = os.getcwd()
+files = os.listdir(path)
+
+for file in files:
+    if file.endswith(".jpg") or file.endswith(".png"):
+        os.makedirs("Images", exist_ok=True)
+        shutil.move(file, "Images/" + file)
+
+    elif file.endswith(".pdf") or file.endswith(".docx"):
+        os.makedirs("Documents", exist_ok=True)
+        shutil.move(file, "Documents/" + file)
+
+    elif file.endswith(".mp4"):
+        os.makedirs("Videos", exist_ok=True)
+        shutil.move(file, "Videos/" + file)
+
+print("Files organized successfully!")

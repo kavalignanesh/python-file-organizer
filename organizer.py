ab7cca8d018e08238cf146974f7bda0168619346
Gnanesh kavali <kavalignanesh@gmail.com>
2026-04-23 14:53:55 +0530

    improved automation logic

--- organizer.py
+++ organizer.py
@@ -4,18 +4,24 @@ import shutil
 path = os.getcwd()
 files = os.listdir(path)
 
+file_types = {
+    "Images": [".jpg", ".png", ".jpeg"],
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Videos": [".mp4", ".mkv"]
+}
+
 for file in files:
-    if file.endswith(".jpg") or file.endswith(".png"):
-        os.makedirs("Images", exist_ok=True)
-        shutil.move(file, "Images/" + file)
+    for folder, extensions in file_types.items():
+        if any(file.endswith(ext) for ext in extensions):
+            os.makedirs(folder, exist_ok=True)
 
-    elif file.endswith(".pdf") or file.endswith(".docx"):
-        os.makedirs("Documents", exist_ok=True)
-        shutil.move(file, "Documents/" + file)
+            destination = os.path.join(folder, file)
 
-    elif file.endswith(".mp4"):
-        os.makedirs("Videos", exist_ok=True)
-        shutil.move(file, "Videos/" + file)
+            if not os.path.exists(destination):
+                shutil.move(file, destination)
+                print(f"Moved {file} → {folder}")
+            else:
+                print(f"Skipped duplicate: {file}")
 
-print("Files organized successfully!")
-print("Automation completed 🚀")
+print("Smart automation complete 🚀")
+
